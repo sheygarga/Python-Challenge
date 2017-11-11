b58faef698e9eb2a14a774cf1288ef70270f16ea
Shreyas Garga <shreyasgarga@gmail.com>
2017-11-11 18:55:47 -0500

    git commit -m

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,87 @@
-#can use panda
-#work on this one
+
+# coding: utf-8
+
+# In[64]:
+
+
+import pandas as pd
+import numpy
+
+
+# In[22]:
+
+
+file1_df = pd.read_csv('election_data_1.csv')
+
+
+# In[31]:
+
+
+total = file1_df['Voter ID'].count()
+total
+
+
+# In[23]:
+
+
+file1_df['Candidate'].unique()
+
+
+# In[24]:
+
+
+file1_votes = file1_df['Candidate'].value_counts()
+file1_votes
+
+
+# In[25]:
+
+
+print('Election Results')
+
+
+# In[34]:
+
+
+print('-------------------------')
+print('Total Votes: '+ str(total))
+print('-------------------------')
+
+
+# In[75]:
+
+
+candidates = file1_df['Candidate'].unique()
+candidates = candidates.tolist()
+type(candidates)
+
+
+# In[76]:
+
+
+votes = file1_votes.tolist()
+type(votes)
+
+
+# In[81]:
+
+
+pair = dict(zip(candidates,votes))
+#print(pair)
+
+
+# In[85]:
+
+
+for (c,v) in pair.items():
+    percent = (v/total)*100
+    print (c +": "+str(percent)+'% ('+str(v)+')')
+
+
+# In[90]:
+
+
+print('-------------------------')
+print('Winner: '+next(iter(pair)))
+print('-------------------------')
 
